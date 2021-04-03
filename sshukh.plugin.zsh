53a9ea38e425f24af0d757950c39538d3c54cbbd
Anatoly Kopyl <akopyl@radner.ru>
2021-04-03 15:37:23 +0300

    Turned everything ito a function

--- sshukh.plugin.zsh
+++ sshukh.plugin.zsh
@@ -1,14 +1,15 @@
-#!/bin/sh
-output=$(ssh "$@")
-if [ $? -eq 255 ];
-then
+sshukh () {
+  output=$(ssh "$@")
+  if [ $? -eq 255 ];
+  then
     host=$(cut -d'@' -f2 <<< $1)
     while true; do
-        read -p "Update known_hosts? [y/n] " yn
-        case $yn in
-            [Yy]* ) ssh-keygen -R $host && ssh "$@"; break;;
-            [Nn]* ) exit;;
-            * ) echo "Please answer y or n.";;
-        esac
+      read -p "Update known_hosts? [y/n] " yn
+      case $yn in
+        [Yy]* ) ssh-keygen -R $host && ssh "$@"; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer y or n.";;
+      esac
     done
-fi
+  fi
+}
