e93381f3a6b96b1ef615e7adad6cf949000228a6
Anatoly Kopyl <akopyl@radner.ru>
2021-04-03 16:19:23 +0300

    Fixed the prompt

--- sshukh.plugin.zsh
+++ sshukh.plugin.zsh
@@ -1,12 +1,27 @@
+# ------------------------------------------------------------------------------
+# Description
+# -----------
+#
+# User will be prompted if they want to update known_hosts if ssh errors out 
+# with "Host key verification failed."
+#
+# ------------------------------------------------------------------------------
+# Authors
+# -------
+#
+# * Anatoly <akopyl@radner.ru>
+#
+# ------------------------------------------------------------------------------
+
 sshukh () {
-  output=$(ssh "$@")
+  output=$(\ssh "$@")
   if [ $? -eq 255 ];
   then
     host=$(cut -d'@' -f2 <<< $1)
     while true; do
-      read -p "Update known_hosts? [y/n] " yn
+      read yn"?Update known_hosts? [y/n] "
       case $yn in
-        [Yy]* ) ssh-keygen -R $host && ssh "$@"; break;;
+        [Yy]* ) ssh-keygen -R $host && \ssh "$@"; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer y or n.";;
       esac
