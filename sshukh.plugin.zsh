358880697ed27da7ef926060347a9c4013b1d487
Anatoly Kopyl <akopyl@radner.ru>
2021-04-03 15:17:27 +0300

    Initial commit

---
+++ sshukh.plugin.zsh
@@ -0,0 +1,14 @@
+#!/bin/sh
+output=$(ssh "$@")
+if [ $? -eq 255 ];
+then
+    host=$(cut -d'@' -f2 <<< $1)
+    while true; do
+        read -p "Update known_hosts? [y/n] " yn
+        case $yn in
+            [Yy]* ) ssh-keygen -R $host && ssh "$@"; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer y or n.";;
+        esac
+    done
+fi
